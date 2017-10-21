9edaf9e83fd3cc578e3748ba98ef1f9edf04ae40
Factorio <factorio@admin.com>
2017-10-21 23:18:56 +0200

    Added web again and changed gitignore because git doesnt know how to asterix

---
+++ web/admin/status.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+<meta http-equiv="refresh" content="1; URL=/admin/status.php">
+</head>
+<body>
+Status:
+<?php
+        echo(file_get_contents("/home/factorio/server/status"));
+?>
+</body>
+</html>
+~
+~
+~
