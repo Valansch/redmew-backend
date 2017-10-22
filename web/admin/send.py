65f21621a7d2d9c07aec45e3c602cdd315ed0047
Factorio <factorio@admin.com>
2017-10-22 23:25:50 +0200

    implemented send.py

---
+++ web/admin/send.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import sys
+from socket import socket, AF_INET, SOCK_DGRAM
+
+if len(sys.argv) > 2:
+
+        mySocket = socket( AF_INET, SOCK_DGRAM )
+
+        PORT_NUMBER = int(sys.argv[2])
+        SIZE = 1024
+        mySocket.sendto( bytes(sys.argv[1], 'UTF-8') , ('127.0.0.1', PORT_NUMBER) )
+
