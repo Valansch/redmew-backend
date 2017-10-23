42bb71e79f3b13b1c8fc842d3e3328a6c2b0ae91
TWLTriston <triston@gmail.com>
2017-10-22 20:19:11 -0400

    Stream the file

--- web/admin/console-log.php
+++ web/admin/console-log.php
@@ -9,10 +9,23 @@
 $Server = new FactorioServer();
 
 if ( isset($_GET["all"] ) ) {
-   $currentLog = $Server->getLogDownload();
+   $path = $Server->getLogDownload();
 
-   header("content-type: text/plain");
-   echo ( file_get_contents( $currentLog ));
+   $public_name = basename($path);
+
+   // get the file's mime type to send the correct content type header
+   $finfo = finfo_open(FILEINFO_MIME_TYPE);
+   $mime_type = finfo_file($finfo, $path);
+
+   // send the headers
+   header("Content-Disposition: inline; filename=$public_name;");
+   header("Content-Type: $mime_type");
+   header('Content-Length: ' . filesize($path));
+
+   // stream the file
+   $fp = fopen($path, 'rb');
+   fpassthru($fp);
+
 } else {
    $currentLog = $Server->getLog();
    print implode("<br />", explode( "\n", $currentLog) );
