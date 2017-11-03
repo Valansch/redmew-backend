2a01260c094d901d3a1ef082800ddf3c99b14605
Factorio <factorio@admin.com>
2017-11-03 12:17:01 +0100

    Added password form to make_passwords

--- web/admin/make_password.php
+++ web/admin/make_password.php
@@ -1,2 +1,14 @@
+<head>
+<script>
+	function redirect() {
+		var URL = window.location + "?password=" + document.getElementById("pw").value;
+		window.location.replace(URL);
+	}
+</script>
+</head>
 <?php
-var_dump( password_hash("redmew", PASSWORD_DEFAULT));
+if ($_GET['password'] != null) {
+	var_dump(password_hash($_GET['password'], PASSWORD_DEFAULT));
+} else {
+	echo('Password: <input type="input" id="pw"/>	<input type="submit" onclick="redirect()" />');
+}
