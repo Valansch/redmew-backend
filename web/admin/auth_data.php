aa908a3fe875a3eade47e9f2ac0fc77393ae28b9
Factorio <factorio@admin.com>
2017-11-03 13:08:10 +0100

    Added altaric and red

--- web/admin/auth_data.php
+++ web/admin/auth_data.php
@@ -5,4 +5,6 @@
 $auth_admins = array(
    "triston" => '$2y$10$h0XIIXHIUhK6.igGqG0vteUSkwwPLqiLteIP5xTb8L9.uuALfv/qC',
    "valansch" => '$2y$10$fK2Hdly/B7jopZIwOHxFTO2W89qgaDqRXW0adfTOWDBCa7gxV7Fni',
+   "altaric" => '$2y$10$1yF7j6CeKwZejO7NNQzMCOEtV1L3CO4y115h6OLh17NWAhs6nwPom',
+   "red" = '$2y$10$caXsyfVTPQwyJoU57HqbBupPowhBk/iNgNj5ld1v2hfMb0QC146PG',
 );
