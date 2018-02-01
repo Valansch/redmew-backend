734a881d607946fb74992df1871b26d37d323ff6
Factorio <factorio@admin.com>
2018-02-01 21:42:25 +0100

    Implemented chat commands

--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# vim: set noexpandtab ts=8 sw=8 sts=8 number:
+
 import socket
 import re
 import os
@@ -11,16 +13,21 @@ import random
 from subprocess import Popen
 from time import gmtime, strftime
 
+
 # Open chatlog file
 chatlog_file = 'log/log.log'
 port_file_path = 'control_port'
 port_file = open(port_file_path, 'r')
 chatlog = open(chatlog_file, 'r')
 chatlog.seek(0, 2) # Go to the end of the file
-
+api_response_timeout = 3 # seconds
+channel = None
 # Init socket connection to factorio console
 PORT = 0
 cwd = os.path.dirname(os.path.abspath(__file__))
+bantype = "ban"
+
+
 with open(cwd + "/control_port", 'r') as f:
 	PORT = int(f.readlines()[0])
 	print("Port: " + str(PORT))
@@ -37,6 +44,87 @@ lastpass = totp.now()
 
 # Store the candidate :D
 pending_ban = None
+
+class Command:
+	is_runable = False
+	def __init__(self, name, help_text, implemented=True, output = False, is_function = True, num_args = 0, admin = False):
+		self.name = name
+		self.help_text = help_text
+		self.implemented = implemented
+		self.output = output
+		self.is_function = is_function
+		self.num_args = num_args
+		self.admin = admin
+
+	def run(self, msgid, admin):
+		if self.name == "help":
+			return commands.print_help()
+		#check credentials
+		if not admin and self.admin: raise RuntimeError("You don't have permission to run this command")	
+		if self.is_function:
+			first_fct_arg = '"' + str(msgid) + '",' if self.output else ","
+			template = "/silent-command " + self.name + '(' + first_fct_arg	
+			for i in range(1,self.num_args):
+				template = template + '"' + call[i] + '",'
+			template = template[:-1]
+			template = template + ")"
+		else:
+			template = "/" + " ".join(self.args)
+		sckt.sendto(template.encode(), dest)
+		if not self.output: return ""			
+		filename = cwd + "/script-output/api/" + str(msgid)
+		tries = 0
+		while not os.path.isfile(filename):
+			if tries > api_response_timeout * 10:
+				return None
+			asyncio.sleep(0.1)
+			tries +=1
+		response = ""
+		with open(filename, "r") as f:
+			response = response + f.readline()
+		print(response)
+		return response
+	
+	def make_runable(self, args):
+		runable = Command(self.name, self.help_text, self.implemented, self.output, self.is_function, self.num_args, self.admin)
+		runable.args = args
+		runable.is_runable = True
+		return runable
+class Commands:
+	commands = []
+	def add(self, command):
+		self.commands.append(command)
+
+	def get_command(self, name):
+		for c in self.commands: 
+			if c.name == name: return c 
+		return None
+	
+	def parse(self, command_str):
+		if command_str == "": raise SyntaxError("Empty command.")
+		args = command_str.split()
+		cmd = self.get_command(args[0])
+		if cmd is None: raise SyntaxError("Command not found.")
+		if not cmd.implemented: raise SyntaxError("Command not implemented.")
+		if not len(args) - 1 == cmd.num_args: raise SyntaxError("{} arguments expected, got {}".format(cmd.num_args,len(args) - 1))
+		return cmd.make_runable(args)
+		
+	
+	def print_help(self):
+		out = ""
+		for c in self.commands:
+			out = out + "/" + c.name + " " + c.help_text + "\n"
+		return out
+
+ 
+commands = Commands()
+commands.add(Command("help", "Prints this help text"))
+commands.add(Command("spy", "spys on a player", implemented=False, num_args=1, is_function = False))
+commands.add(Command("players", "lists all online players.",implemented=False, output=True))
+commands.add(Command("spy", "spies on a player.", is_function=False, num_args=1, admin=True))
+commands.add(Command("ban", "bans a player.", is_function=False, num_args=1 , admin=True))
+commands.add(Command("unban","unbans a player.", is_function=False, num_args=1, admin=True))
+commands.add(Command("time", "How lomg the server has been running.", implemented=False,is_function=False))
 
 def print_to_file(str):
 	with open(cwd + "/log/bot.log", "a") as f:
@@ -88,19 +176,23 @@ async def send_msg_to_discord(event, msg):
 	print(msg)
 	msg = re.sub(r"@(\S+)", replace_mentions, msg)
 	server = client.get_server('312150126766456832') # redmew server
-	channel = discord.Object(id='356780115159547914') # ingame-chat
+	global channel
+	if not channel:
+		channel = discord.Object(id='356780115159547914') # ingame-chat
 	if event == "JOIN":
 		embed = discord.Embed(title=msg.upper(), color=discord.Color(random.randint(0, 0xFFFFFF)))
-		async with aiohttp.request('GET', 'http://random.cat/meow') as resp:
-			if resp.status == 200:
-				cat_img = await resp.json()
-				embed.set_image(url=cat_img["file"])
+		embed.set_image(url="https://picsum.photos/400/300/?random")
+		# async with aiohttp.request('GET', 'http://random.cat/meow') as resp:
+		#	if resp.status == 200:
+		#		cat_img = await resp.json()
+		#		embed.set_image(url=cat_img["file"])
 		sent_msg = await client.send_message(channel, embed=embed)
 		emojis = random.sample(server.emojis, 3)
 		for emo in emojis:
 			await client.add_reaction(sent_msg, emo)
 	else:
 		await client.send_message(channel, msg)
+
 
 def send_msg_to_game(username, msg):
 	msg = re.sub(r'[\n\r\\"\']', '', msg).strip()
@@ -110,16 +202,6 @@ def send_msg_to_game(username, msg):
 		print("%s: %s" % (username, msg))
 		print_to_file("%s %s@discord: %s" % (strftime("%Y-%m-%d %X [CHAT]", gmtime()), username, msg))
 		sckt.sendto(template.encode(), dest)
-
-def spy(username):
-	username = re.sub(r'[\n\r\\"\']', '', username).strip()
-	template = '/spyshot %s' % username
-	sckt.sendto(template.encode(), dest)
-
-def ban(username):
-	username = re.sub(r'[\n\r\\"\']', '', username).strip()
-	template = '/ban %s' % username
-	sckt.sendto(template.encode(), dest)
 
 @client.event
 async def on_ready():
@@ -133,26 +215,27 @@ async def on_message(message):
 	global pending_ban
 	global lastpass
 	if message.channel.name == 'ingame-chat' and not message.author.bot:
-		if message.content.startswith("/spy"):
-			matches = re.match("^/spy (.*)", message.content)
-			if matches and matches.group(1):
-				spy(matches.group(1))
+		if message.clean_content.startswith("/"):
+			print(message.author.name + ": " + message.clean_content)
+			try:
+				command = commands.parse(message.clean_content[1:])
+			except SyntaxError as e:
+				await client.send_message(message.channel, e)
 				return
-		if message.content.startswith("/ban"):
-			matches = re.match("^/ban (.*)", message.content)
-			if matches and matches.group(1):
-				pending_ban = matches.group(1)
-				await client.send_message(message.channel, 'Enter your password to ban user {}.'.format(pending_ban))
+			admin = False
+			for role in message.author.roles:
+				if role.name == "admins": admin = True
+			try:
+				result = command.run(message.id, admin)
+			except RuntimeError as e:
+				await client.send_message(message.channel, e)
 				return
-		if re.match("^[0-9]{6}$", message.content):
-			if pending_ban != None:
-				if message.content == totp.now() and message.content != lastpass:
-					ban(pending_ban)
-					pending_ban = None
-					lastpass = message.content
-				else:
-					await client.send_message(message.channel, 'Wrong password.')
-				return
+			if result is None:
+				await client.send_message(message.channel, "Server did not responde.")
+			elif not result == "":
+				await client.send_message(message.channel, result)
+
+			return
 		send_msg_to_game(message.author.name, message.clean_content)
 
 # Run the event loop
