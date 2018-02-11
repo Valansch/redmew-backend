d684d0eca367a2333689cce7dcebdbb3f638fec6
Factorio <factorio@admin.com>
2018-02-11 14:08:49 +0100

    added new admins

--- web/admin/auth_data.php
+++ web/admin/auth_data.php
@@ -8,7 +8,7 @@ $auth_admins = array(
    "altaric" => '$2y$10$1yF7j6CeKwZejO7NNQzMCOEtV1L3CO4y115h6OLh17NWAhs6nwPom',
    "redlabel" => '$2y$10$caXsyfVTPQwyJoU57HqbBupPowhBk/iNgNj5ld1v2hfMb0QC146PG',
    "sanctorio" => '$2y$10$BmjvpsLgnDMy7VXAjfy6VOX3F71ldfQf2DcCX1TNs6dLQX2/mbQnS',
-   "diffiehellman" => '$2y$10$BmjvpsLgnDMy7VXAjfy6VOX3F71ldfQf2DcCX1TNs6dLQX2/mbQnS',
+   "diffie" => '$2y$10$bc2bEZ2M8DDPKcjkmMalPOFFmMfsmDq3gcgqbwHQVNi3cHEM8gsaS',
    "gynox" => '$2y$10$B9S0zF184lUw1jk7uzjf9Obf8CvlMqsPzxApCqNox/2vsSfHHVOCm',
    "dinodod" => '$2y$10$m9yoPp54DrFkdig57ks0YOGBQ.14ydzjBN9hE6D3JjjKosjsBO1Qu',
    "air20" => '$2y$10$fdYjIe4SbHyueIAw6W81yuZUzm2fvbqH6XNbPisFNhHivZrpV5mFS',
@@ -16,4 +16,6 @@ $auth_admins = array(
    "redlabel" => '$2y$10$9aDcpDgXOezgy/SwCqgx2uycU.mszGCPdN4TfsOzA9.sK7U9rT8dC',
    "i1728" => '$2y$10$qYq1NhJJPQ.A0/Q2EyCy/.yUQ4ToM/fTwEspdce8ZBPJVjDM4lpnC',
    "grilledham" => '$2y$10$aEa8X7R/5g3.tv0CQT49WOlE98tKAjPrxgPmGyHcLy0l3bfNs6RDq',
+   "xatev" => '$2y$10$u0IRCDU97PJtyHX/BgJ8XOALPoEL80oDIHajqxDixrRbz7FEq4Z.u',
+   "cybernoise" => '$2y$10$f3VcI6Wcgr6O2g8vIiLpNujHrhMdjzpjGcLLm7O72MM5qmMwQvp3.',
 );
