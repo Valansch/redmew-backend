c5964333023518faea389f9aadb82cf7bf0a5e1f
Factorio <factorio@admin.com>
2018-06-10 21:05:48 +0200

    added oranges new password and removed old admins

--- web/admin/auth_data.php
+++ web/admin/auth_data.php
@@ -5,18 +5,15 @@
 $auth_admins = array(
    "triston" => '$2y$10$h0XIIXHIUhK6.igGqG0vteUSkwwPLqiLteIP5xTb8L9.uuALfv/qC',
    "valansch" => '$2y$10$fK2Hdly/B7jopZIwOHxFTO2W89qgaDqRXW0adfTOWDBCa7gxV7Fni',
-   "orange" => '$2y$10$Z6OjEcDQAMIOoTkR/bqhpuwM/gsjG7jyezU70reC9V2Q3TA3o147q',
-   "theorangeangle" => '$2y$10$Z6OjEcDQAMIOoTkR/bqhpuwM/gsjG7jyezU70reC9V2Q3TA3o147q',
    "altaric" => '$2y$10$1yF7j6CeKwZejO7NNQzMCOEtV1L3CO4y115h6OLh17NWAhs6nwPom',
    "sanctorio" => '$2y$10$x949OzL3F5ew6zzfG5LIj.SKlIkmBBiZLdeOc8EjropneAECsZ0M2',
    "diffie"    => '$2y$10$x949OzL3F5ew6zzfG5LIj.SKlIkmBBiZLdeOc8EjropneAECsZ0M2',
-   "gynox" => '$2y$10$B9S0zF184lUw1jk7uzjf9Obf8CvlMqsPzxApCqNox/2vsSfHHVOCm',
-   "dinodod" => '$2y$10$m9yoPp54DrFkdig57ks0YOGBQ.14ydzjBN9hE6D3JjjKosjsBO1Qu',
    "air20" => '$2y$10$fdYjIe4SbHyueIAw6W81yuZUzm2fvbqH6XNbPisFNhHivZrpV5mFS',
    "sholvo" => '$2y$10$285KDr/OWdYizPfVhcf/yeeyYazYzgD45kztt2DJJQ9aeK4r1bNHu',
    "i1728" => '$2y$10$qYq1NhJJPQ.A0/Q2EyCy/.yUQ4ToM/fTwEspdce8ZBPJVjDM4lpnC',
    "grilledham" => '$2y$10$aEa8X7R/5g3.tv0CQT49WOlE98tKAjPrxgPmGyHcLy0l3bfNs6RDq',
    "xatev" => '$2y$10$u0IRCDU97PJtyHX/BgJ8XOALPoEL80oDIHajqxDixrRbz7FEq4Z.u',
-   "cybernoise" => '$2y$10$f3VcI6Wcgr6O2g8vIiLpNujHrhMdjzpjGcLLm7O72MM5qmMwQvp3.',
-   "slug" => '$2y$10$dqly4AX3aIEJQQbJwTVY7uMGMbBXAE8OJI2X0T2Rdr30nG.74Z5kK',
+   "orange" => '$2y$10$El.hs4yWFlxVuO/QECjEG.AlM0qcar2fvoaWrKm4fdEHlTzunvbEy',
+   "theorangeangle" => '$2y$10$El.hs4yWFlxVuO/QECjEG.AlM0qcar2fvoaWrKm4fdEHlTzunvbEy',
+
 );
