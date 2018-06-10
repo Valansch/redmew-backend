38387bea6e917d99b2c9769edcdadc09c96ca392
Factorio <factorio@admin.com>
2018-06-10 11:03:15 +0200

    Added ls saves styles and display

--- web/admin/index.php
+++ web/admin/index.php
@@ -32,7 +32,8 @@ if (!$validated) {
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
    <link rel="stylesheet" type="text/css" href="factorio.css">
    <link rel="stylesheet" type="text/css" href="redmew.css">
-
+   <meta name='viewport' content='width=device-width, initial-scale=1'>
+	
    <script type="text/javascript">
    // var window.server_status;
    var  serverControlTimeout = null;
@@ -43,6 +44,8 @@ if (!$validated) {
    $(document).ready(function () {
       reloadStatus();
       reloadConsole();
+      lsSaves("name");
+      setSelectionButtonsEnabled(false);
       $("#command").focus();
       $("#description").hide();
       $("#saveDescriptionBox").hide();
@@ -111,6 +114,12 @@ if (!$validated) {
       $("#upload_select").change(function (){
         $("#btnUpload").click();	
       });
+
+      $("#deleteSel").click(function (e){
+     	if (confirm("Do you really want to delete " + saves[selectedSave]["name"] + "?")){
+	
+	}
+      });
    });
 
    function serverControlButtonEnable() {
@@ -142,7 +151,7 @@ if (!$validated) {
    function reloadConsole() {
       window.clearTimeout(serverConsoleTimeout);
       var lines = parseInt($("#numLines").html());
-      var url = "console.log.php";
+      var url = "console-log.php";
       if (!isNaN(lines)) {
         url = "console-log.php?lines=" + lines
       }
@@ -153,7 +162,68 @@ if (!$validated) {
 
    }
 
-   function init_controls() {
+   function tdn(n) {
+      if (n < 10)
+         return "0" + n;
+      return n;
+   }
+
+   function formatTime(dateTime){
+      return dateTime.getFullYear() + "-" +
+      tdn(dateTime.getMonth()) + "-" +
+      tdn(dateTime.getDate()) + " " +
+      tdn(dateTime.getHours()) + ":" +
+      tdn(dateTime.getMinutes()) + ":" +
+      tdn(dateTime.getSeconds());
+   }
+
+   function setSelectionButtonsEnabled(status) {
+      $("#deleteSel").attr("disabled", !status);
+      $("#loadSel").attr("disabled", !status);
+      $("#renameSel").attr("disabled", !status);
+      $("#moveSel").attr("disabled", !status); 
+ 
+   }
+   function rowClick(id){
+   	setSelectionButtonsEnabled(true);
+   	$("#row" + selectedSave).removeClass('active');
+   	selectedSave = id;
+   	$("#row" + selectedSave).addClass('active');
+	console.log(saves[id]["name"]);
+   }
+
+	function sortByKey(a, key, reverse) {
+		return a.sort(function(a, b) {
+			var x = a[key]; var y = b[key];
+			if (reverse)
+				return ((x > y) ? -1 : ((x < y) ? 1 : 0));
+			else
+				return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+		});
+	}
+
+   saves = {};
+   selectedSave = null;
+   sortKeyReversed = {};
+   sortKeyReversed["name"] = true;
+   sortKeyReversed["timestamp"] = true;
+   sortKeyReversed["size"] = true;
+   function lsSaves(sortKey) {
+      sortKeyReversed[sortKey] = !sortKeyReversed[sortKey];
+      $.get("saves-ls.php", function (data) { 
+	var savesJson = JSON.parse(data);
+	saves = sortByKey(savesJson, sortKey, sortKeyReversed[sortKey]);
+	$("#saveSelectionBody").empty();
+	for(let i = 0; i < saves.length; i++) {
+	   let size = Math.floor(saves[i]["size"] / 10485.76) / 100 + "MB";
+	   let time = formatTime(new Date(saves[i]["timestamp"] * 1000)); 
+	   $("#saveSelectionBody").append('<tr class="tblRow" id="row' + i + '" onclick="rowClick(' + i + ')"><td>' + saves[i]["name"] + '</td><td>' + size + '</td><td>' + time + '</td></tr>');
+	}
+      });
+   }
+
+
+   function init_controls() { 
       $("#controlPID").html( window.server_status.control_pid + " / " + window.server_status.control_port);
       $("#factorioPID").html( window.server_status.factorio_pid + " / " + window.server_status.factorio_status );
       $("#factorioName").html( window.server_status.serverSettings.name );
@@ -190,54 +260,80 @@ if (!$validated) {
 </head>
 <body>
    <h1>redmew: Factorio Server Control</h1>
+   <div id="serverControlSaveSelectionWrapper">
+      <div id="serverControl">
+      	<h2>Server Command and Control</h2>
+      	<dl>
+        	<dt>Control Script</dt>
+         	<dd id="controlPID"></dd>
+         	<dt>Factorio</dt>
+         	<dd id="factorioPID"><dd>
+         	<dt>Server Name</dt>
+         	<dd id="factorioName"></dd>
+         	<dt>Description</dt>
+         	<dd id="factorioDescription"></dd>
+         	<dt>Tags</dt>
+         	<dd id="factorioTags"></dd>
+      	</dl>
+      	<ul>
+        	<li id="factorioStart"><a href="send-control.php?control=start" class="btn btn-custom btn-large btn-block">Start</a></li>
+        	<li id="factorioStop"><a href="send-control.php?control=stop" class="btn btn-custom btn-large btn-block">Stop</a></li>
+       		<li id="factorioRestart"><a href="send-control.php?control=restart" class="btn btn-custom btn-large btn-block">Restart</a></li>
+        	<li id="factorioUpdate"><a href="send-control.php?control=update" class="btn btn-custom btn-large btn-block">Update</a></li>
+        	<li id="factorioSave"><a href="send-control.php?control=save" class="btn btn-custom btn-large btn-block">Save</a></li>
+        	<li id="factorioLoad"><a href="" id="upload_link" class="btn btn-custom btn-large btn-block">Load</a></li>
+      	</ul>
+      	<form id="upload_form" action="upload.php" method="post" enctype="multipart/form-data">
+         	<input id="upload_select" type="file" name="fileToUpload" />
+        	<input type="submit" value="Upload Image" name="submit" id="btnUpload" />
+     	</form>
+	
+	<div id="help-StartTMux">
+        	<strong>Server control session not found. Please start the tmux using:</strong>
+        	<xmp></xmp>
+      	</div>
 
-   <div id="serverControl">
-      <h2>Server Command and Control</h2>
-      <dl>
-         <dt>Control Script</dt>
-         <dd id="controlPID"></dd>
-         <dt>Factorio</dt>
-         <dd id="factorioPID"><dd>
-         <dt>Server Name</dt>
-         <dd id="factorioName"></dd>
-         <dt>Description</dt>
-         <dd id="factorioDescription"></dd>
-         <dt>Tags</dt>
-         <dd id="factorioTags"></dd>
-      </dl>
-      <ul>
-         <li id="factorioStart"><a href="send-control.php?control=start" class="btn btn-custom btn-large btn-block">Start</a></li>
-         <li id="factorioStop"><a href="send-control.php?control=stop" class="btn btn-custom btn-large btn-block">Stop</a></li>
-         <li id="factorioRestart"><a href="send-control.php?control=restart" class="btn btn-custom btn-large btn-block">Restart</a></li>
-         <li id="factorioUpdate"><a href="send-control.php?control=update" class="btn btn-custom btn-large btn-block">Update</a></li>
-         <li id="factorioSave"><a href="send-control.php?control=save" class="btn btn-custom btn-large btn-block">Save</a></li>
-         <li id="factorioLoad"><a href="" id="upload_link" class="btn btn-custom btn-large btn-block">Load</a></li>
-      </ul>
-      <form id="upload_form" action="upload.php" method="post" enctype="multipart/form-data">
-          <input id="upload_select" type="file" name="fileToUpload" />
-         <input type="submit" value="Upload Image" name="submit" id="btnUpload" />
-      </form>
+ 
+      </div>
+  
 
 
-      <div id="help-StartTMux">
-         <strong>Server control session not found. Please start the tmux using:</strong>
-         <xmp></xmp>
+   <div id="saveSelectionWrapper"> 
+      <a class="btn btn-custom btn-small" id="deleteSel" >Delete</a> 
+      <a class="btn btn-custom btn-small" id="loadSel">Load</a> 
+      <a class="btn btn-custom btn-small" id="renameSel">Rename</a> 
+      <a class="btn btn-custom btn-small" id="moveSel">Move to archive</a> 
+      <div id="saveSelection" class="text_box">
+         <table>
+	 	<thead><tr>
+         		<th onclick="lsSaves('name')">Save File</th>
+      	 		<th onclick="lsSaves('size')">Size</th>
+	 		<th onclick="lsSaves('name')">Last Modified</th>
+	 	</tr><thead>
+		<tbody id="saveSelectionBody">
+		</tbody>
+	 </table>
       </div>
    </div>
-   <div id = "description_box">
-      <h3>
-         [<a id="toggle_description" href="#">Edit Description</a>]
-	 <div style="display: inline" id="saveDescriptionBox">[<a id="saveDescription" href="#">Save</a>]</div>
-      </h3>
-      <div contentEditable class="text_box" id="description"></div>
    </div>
-   <div id="dummy_output"></div>
-   <div id="console">
-      <h2>Server Console</h2>
-      [ <a href="console-log.php?all=all" target="_blank">Full Log</a> ]  
-      [ Lines: <div contentEditable class="text_box lines" id="numLines">20</div> ]
-      <p></p>
-      <div class="output text_box"></div>
+    <div id="console">
+
+
+	<div id = "description_box">
+		<h3>
+			[<a id="toggle_description" href="#">Edit Description</a>]
+			 <div style="display: inline" id="saveDescriptionBox">[<a id="saveDescription" href="#">Save</a>]</div>
+      		</h3>
+		<div contentEditable class="text_box" id="description"></div>
+	</div>
+   	<div id="dummy_output"></div>
+	<h2>Server Console</h2>
+	[ <a href="console-log.php?all=all" target="_blank">Full Log</a> ]  
+	[ Lines: <div contentEditable class="text_box lines" id="numLines">20</div> ]
+	<p></p>
+
+
+     <div class="output text_box"></div>
       <form id="command_form" method="post" action="send-command.php">
          <?php print $_SERVER['PHP_AUTH_USER']; ?>: <input type="text" autocomplete="off" id="command" name="command" />
          <input type="submit" value="Send" />
